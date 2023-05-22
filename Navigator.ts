c3d999407d2dd3e562bde19f3f596e3b3fd12afe
Darya Zakhar <daryazakhar.89@gmail.com>
2023-05-22 09:56:34 +0400

    feat: add navigation

---
+++ Navigator.ts
@@ -0,0 +1,35 @@
+import { mouse, straightTo, centerOf } from "@nut-tree/nut-js";
+
+export class Navigator {
+
+  static async mouseUp(step: number) {
+    const mousePoint = await mouse.getPosition();
+    mousePoint.y -= step;
+    await mouse.setPosition(mousePoint);
+  }
+
+  static async mouseDown(step: number) {
+    const mousePoint = await mouse.getPosition();
+    mousePoint.y += step;
+    await mouse.setPosition(mousePoint);
+  }
+
+  static async mouseLeft(step: number) {
+    const mousePoint = await mouse.getPosition();
+    mousePoint.x -= step;
+    await mouse.setPosition(mousePoint);
+  }
+
+  static async mouseRigth(step: number) {
+    const mousePoint = await mouse.getPosition();
+    mousePoint.x += step;
+    await mouse.setPosition(mousePoint);
+  }
+
+  static async getMousePosition() {
+    const mousePoint = await mouse.getPosition();
+
+    return `${mousePoint.x} px, ${mousePoint.y} px`.replace(/ /g, "_");
+  }
+
+}
